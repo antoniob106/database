2364e107ca412d6b464227bfd948e01ee45f67a4
antoniob106 <antoniobalbuena12345@gmail.com>
2026-04-29 16:46:08 +0000

    added comments to room.java

--- room.java
+++ room.java
@@ -1,10 +1,12 @@
 class room {
     public static void main(String[] args) {
+        // every possible room, floor, and wing in the school
         int[] rooms = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         String[] floors = {"B","1","2","3","4","5","6","7","8"};
         String[] wings = {"N","S","E","W"};
         String create = "INSERT INTO Room (RoomName) VALUES ";
-
+        
+        // add every possible combination of elements from rooms, floors, and wings as mysql statements to add to the Room table
         for (String floor : floors) {
             for (String wing : wings) {
                 for (int room : rooms) {
