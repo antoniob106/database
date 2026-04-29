d0b9f813a53ef3d06f55924d0619c019fa445ed0
BedguyHeck <113556458+BedguyHeck@users.noreply.github.com>
2026-04-29 14:00:08 -0400

    Change file path from 'data.txt' to 'data'

--- Instructor.java
+++ Instructor.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Instructor {
     public static void main(String[] args) {
-        File myFile = new File("data.txt");
+        File myFile = new File("data");
         List<String> dataList = new ArrayList<>();
         try {
             Scanner scanner = new Scanner(myFile);            
