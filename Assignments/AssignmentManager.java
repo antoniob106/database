a82a9c0b40517226e89b299859cdcf89430f522c
antoniob106 <antoniobalbuena12345@gmail.com>
2026-05-13 17:35:17 +0000

    assignmentmanager added

--- Assignments/AssignmentManager.java
+++ Assignments/AssignmentManager.java
@@ -4,47 +4,46 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.lang.Math;
 
 public class AssignmentManager {
     public static void main(String[] args) {
-        File myFile = new File("Minors.txt");
-        List<Minor> minorList = new ArrayList<>();
-        int currentID = 1;
+        List<String> classList = new ArrayList<>(List.of("A", "B", "C")); // placeholder value
+        File myFile = new File("Assignments/Assignments.txt");
+        for (int i = 0; i < classList.size(); i++) {
+            List<Assignment> assignmentList = new ArrayList<>();
+            int currentID = 0;
+
+            try {
+                Scanner scanner = new Scanner(myFile);
+                while (scanner.hasNextLine()) {
+                    currentID++;
+                    String[] line = scanner.nextLine().split("\\|");
+                    assignmentList.add(new Assignment(line[0], line[1], currentID, i));
+            }
+            scanner.close();
+            System.out.println("Loaded " + assignmentList.size() + " assignments.");
+            assignmentList.forEach(System.out::println);
+            } catch (FileNotFoundException e) {
+                System.out.println("Error: File not found.");
+            }
+        }
     }
-    {
-        File myFile = new File("Majors.txt");
-        List<Major> majorList = new ArrayList<>();
-        int currentID = 1;
+}
+
+class Assignment {
+    String name;
+    String type;
+    int ID;
+    int classID;
+
+    public Assignment(String name, String type, int ID, int classID) {
+        this.name = name;
+        this.type = type;
+        this.ID = ID;
+        this.classID = classID;
     }
-
-//         try {
-//             Scanner scanner = new Scanner(myFile);
-//             while (scanner.hasNextLine()) {
-//                 String name = scanner.nextLine();
-//                 assignmentList.add(new Assignment(name, currentID++));
-//             }
-//             scanner.close();
-//             System.out.println("Loaded " + assignmentList.size() + " assignments.");
-//             assignmentList.forEach(System.out::println);
-//         } catch (FileNotFoundException e) {
-//             System.out.println("Error: File not found.");
-//         }
-//     }
-// }
-
-
-// class Assignment {
-//     String name;
-//     int ID;
-
-
-//     public Assignment(String name, int ID) {
-//         this.name = name;
-//         this.ID = ID;
-//     }
-//     @Override
-//     public String toString() {
-//         return String.format("[%03d] %s", ID, name);
-//     }
-// }
+        @Override
+    public String toString() {
+        return String.format("%s [%s][%d] [%03d]", name, type, ID, classID);
+    }
+}
